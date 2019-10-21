e5b95c09f634c4886348c82a08da3697f11cb491
Yoann Le Fur <yoannlefur@orange.fr>
2019-10-22 00:08:52 +0200

    création des formulaires d'inscriptions et d'utilisation

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -5,33 +5,45 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
  */
-class Users
+class Users implements UserInterface, \Serializable
 {
     /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
+     * @ORM\Id
      * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=64)
+     * @Assert\NotBlank()
      */
     private $last_name;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=64)
+     * @Assert\NotBlank()
      */
     private $first_name;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=64, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Email()
      */
     private $email;
+
+    /**
+     * @ORM\Column(type="string", length=64, unique=true)
+     * @Assert\NotBlank()
+     */
+    private $username;
 
     /**
      * @ORM\Column(type="string", length=255)
@@ -62,6 +74,7 @@ class Users
     {
         $this->forgotPasswords = new ArrayCollection();
         $this->orders = new ArrayCollection();
+        $this->setCreationDate(new \DateTime());
     }
 
     public function getId(): ?int
@@ -69,28 +82,47 @@ class Users
         return $this->id;
     }
 
-    public function getLastName(): ?string
+    /**
+     * @return mixed
+     */
+    public function getLastName()
     {
         return $this->last_name;
     }
 
-    public function setLastName(string $last_name): self
+    /**
+     * @param mixed $last_name
+     */
+    public function setLastName($last_name): void
     {
         $this->last_name = $last_name;
-
-        return $this;
     }
 
-    public function getFirstName(): ?string
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
     {
         return $this->first_name;
     }
 
-    public function setFirstName(string $first_name): self
+    /**
+     * @param mixed $first_name
+     */
+    public function setFirstName($first_name): void
     {
         $this->first_name = $first_name;
+    }
 
-        return $this;
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
     }
 
     public function getEmail(): ?string
@@ -129,16 +161,23 @@ class Users
         return $this;
     }
 
-    public function getRoles(): ?Roles
+    /**
+     * Returns the roles granted to the user.
+     *
+     *     public function getRoles()
+     *     {
+     *         return ['ROLE_USER'];
+     *     }
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
     {
-        return $this->roles;
-    }
-
-    public function setRoles(?Roles $roles): self
-    {
-        $this->roles = $roles;
-
-        return $this;
+        return ['ROLE_ADMIN'];
     }
 
     /**
@@ -201,5 +240,60 @@ class Users
         }
 
         return $this;
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+    }
+
+    /**
+     * String representation of object
+     * @link https://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     * @since 5.1.0
+     */
+    public function serialize()
+    {
+        return serialize([
+            $this->id,
+            $this->username,
+            $this->password
+        ]);
+    }
+
+    /**
+     * Constructs the object
+     * @link https://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     * @since 5.1.0
+     */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password
+            ) = unserialize($serialized, ['allowed_classes' => false]);
     }
 }

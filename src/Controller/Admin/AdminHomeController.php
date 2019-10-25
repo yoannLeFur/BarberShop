2e399668b0fdea0c3a968fadd570e200cf7ee041
Yoann Le Fur <yoannlefur@orange.fr>
2019-10-25 19:53:42 +0200

    création de la page d'accueil dans l'administration

---
+++ src/Controller/Admin/AdminHomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller\Admin;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminHomeController extends AbstractController
+{
+
+    /**
+     * @Route("/admin/accueil", name="admin.home")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index()
+    {
+        return $this->render('admin/home.html.twig');
+    }
+}

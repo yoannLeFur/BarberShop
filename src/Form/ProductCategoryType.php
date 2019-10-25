add172b0a75b0d036aafad6f0210081009c34f13
Yoann Le Fur <yoannlefur@orange.fr>
2019-10-26 00:32:59 +0200

    création de la page catégorie avec les fonctionalités new - edit - delete

---
+++ src/Form/ProductCategoryType.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Form;
+
+use App\Entity\Brand;
+use App\Entity\Product;
+use App\Entity\ProductCategory;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductCategoryType  extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => ProductCategory::class,
+            'translation_domain' => 'forms'
+        ));
+    }
+
+}

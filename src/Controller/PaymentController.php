5c2c9f183e22be049be223b927656524c3f0be04
Yoann Le Fur <yoannlefur@orange.fr>
2019-12-08 17:21:10 +0100

    wip stripe

---
+++ src/Controller/PaymentController.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Users;
+use App\Service\Basket\BasketService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PaymentController extends AbstractController
+{
+
+    /**
+     * @Route("/payment", name="payment.index")
+     */
+    public function index(BasketService $basketService)
+    {
+        return $this->render('payment/payment.html.twig', [
+            'user' => $this->getUser(),
+            'items' => $basketService->getFullCart(),
+            'total' => $basketService->getTotal()
+        ]);
+    }
+
+    /**
+     * @Route(path="/payment/validate", name="payment.validate")
+     * @throws \Stripe\Exception\ApiErrorException
+     */
+    public function paymentAction(BasketService $basketService, Request $request)
+    {
+        \Stripe\Stripe::setApiKey('sk_test_VNCNdxiNWEyC6cTDUehVhcEo00X56XFhfg');
+
+        \Stripe\Customer::create([
+            'description' => 'client',
+            'email' => 'yoannlefur@orange.fr'
+        ]);
+
+        $charge = \Stripe\Charge::create([
+            'amount' => $basketService->getTotal(),
+            'source' => $_POST['stripeToken'],
+            'currency' => 'eur',
+            'description' => 'client de mon site BarberShop'
+        ]);
+        return $this->render('basket/index.html.twig', [
+            'charge' => $charge->values(),
+            'items' => $basketService->getFullCart(),
+            'total' => $basketService->getTotal(),
+        ]);
+    }
+}
